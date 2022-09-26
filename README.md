565cfd2a869d44aa4668775ac11472125464dc95
Swapnil Kumar <swapnilskumars@gmail.com>
2022-09-26 17:00:34 +0530

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# counter-app-flask
-Basic counter app using Flask Python
+
+# Counter App
+
+This is a basic counter app made using flask framework in python.
+
+[Live Demo]()
+
+## Installation
+
+
+For Installation follow the following steps
+
+`1. Clone the repository`
+
+`2. cd counter-app-flask`
+
+`3. pip install -r requirements.txt`
+
+`4. python main.py`
+
+`5. Your flask server will start locally`
+    
+## 🔗 Links
+
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/swapnil-singh-01317b21a/)
+
+  
---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, render_template_string
+
+
+app = Flask(__name__, template_folder='static')
+
+database = {"number" : 0}
+
+@app.route('/')
+def hello():
+    return render_template("home_page.html",number = database["number"])
+
+@app.route('/decrement')
+def decrement():
+    database["number"] -= 1
+    return render_template("home_page.html",number = database["number"])
+
+@app.route('/increment')
+def increment():
+    database["number"] += 1
+    return render_template("home_page.html",number = database["number"])
+
+app.run(host="0.0.0.0", port=5000, debug=True)
